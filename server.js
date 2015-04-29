5ec4b38fd14430d31693e6bb454a4d39eb6e4346
Stefan Teixeira <stefanfk@gmail.com>
2015-04-29 18:06:56 -0300

    Adjusting mongoose connection url

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
 require('./config/passport')();
-require('./config/database.js')('mongodb://localhost:27017/fav-organizer');
+require('./config/database.js')('mongodb://127.0.0.1:27017/fav-organizer');
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Server listening on port ' + app.get('port'));
